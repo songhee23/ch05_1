43102d426206932b86a7b43f5f6b7ce57008dc67
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-30 13:32:21 +0900

    useSelector 훅 사용하기

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -4,4 +4,7 @@ import type {AppState} from './AppState'
 const initialAppState = {
   today: new Date()
 }
-export const rootReducer = (state: AppState = initialAppState, action: Action) => state
+export const rootReducer = (prevState: AppState = initialAppState, action: Action) => {
+  const newState = {...prevState} // 깊은 복사 필요
+  return newState
+}

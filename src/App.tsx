66e5927576440fedaeb9127d57e9860d70daf8c2
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-30 11:16:18 +0900

    리덕스로 시계 구현하기 -초기모습

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import type {Action} from 'redux'
 import {Provider as ReduxProvider} from 'react-redux'
-
+import { configureStore } from '@reduxjs/toolkit'
 
 import ReduxClock from './pages/ReduxClock'
 import UseReducerClock from './pages/UseReducerClock'
 
+
+export type AppState = {
+  today:Date
+}
+const initialAppState = {
+  today: new Date()
+}
+const rootReducer = (state:AppState = initialAppState, action:Action) => state
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware()
+})
+
 export default function App() {
 
   return (
+    
+    <ReduxProvider store={store}>
       <main className="p-8">
         <UseReducerClock />
         <ReduxClock />
       </main>
+    </ReduxProvider>
 
   )
 }

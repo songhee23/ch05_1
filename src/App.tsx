d1f6dfa7cb7f6626e5feb5255e56f737f3eed939
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-30 10:52:56 +0900

    중가커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Provider as ReduxProvider} from 'react-redux'
 
-function App() {
+
+import ReduxClock from './pages/ReduxClock'
+import UseReducerClock from './pages/UseReducerClock'
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <main className="p-8">
+        <UseReducerClock />
+        <ReduxClock />
+      </main>
 
-export default App;
+  )
+}
